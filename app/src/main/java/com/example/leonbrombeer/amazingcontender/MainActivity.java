09fa453a6ec5198e7ddba6e540290cfff2fb5bff
leonbrombeer <leon.brombeer@wesustain.com>
2018-09-24 20:02:48 +0200

    ===Initial commit===
    This is the initial commit for $projectName = "Amazing Contender"

---
+++ app/src/main/java/com/example/leonbrombeer/amazingcontender/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.leonbrombeer.amazingcontender;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
+import java.util.Timer;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "73789846b79544b78f8728d8860299a1";
+    private static final String REDIRECT_URI = ""; //TODO rename package structure & add redirect URI
+    private SpotifyAppRemote mSpotifyAppRemote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() { //We will start writing our code here
+        final long START_TIME = System.nanoTime();
+        super.onStart();
+        ConnectionParams connectionParams = new ConnectionParams.Builder(CLIENT_ID)
+                .setRedirectUri(REDIRECT_URI)
+                .showAuthView(true)
+                .build();
+        SpotifyAppRemote.CONNECTOR.connect(this, connectionParams, new Connector.ConnectionListener() {
+            @Override
+            public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                mSpotifyAppRemote = spotifyAppRemote;
+                Log.d("MainActivity", "Connected@"+"! Yay!");
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+                Log.e("MyActivity", throwable.getMessage(), throwable);
+            }
+        });
+    }
+
+    private void connected() {
+        //TODO implement
+    }
+
+    @Override
+    protected void onStop() {
+        SpotifyAppRemote.CONNECTOR.disconnect(mSpotifyAppRemote);
+        super.onStop();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/leonbrombeer/amazingcontender/TimeUtil.java
@@ -0,0 +1,9 @@
+package com.example.leonbrombeer.amazingcontender;
+
+import android.support.annotation.NonNull;
+
+public class TimeUtil {
+
+    public long elapsedTimeInMilliseconds(long startTime)
+
+}
